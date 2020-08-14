b28221e1d4cf87b02c23e0b0d13db3192ef120f9
PhuongThao09 <phuongthao.sq.tuyenquang@gmail.com>
2020-08-14 08:53:19 +0700

    create view

---
+++ resources/views/tasks/tasks.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center my-5">TASK LIST</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    New task
+                </div>
+
+                <div class="card-body">
+                    <form action="/store-todos" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input 
+                                type="text" 
+                                class="form-control" 
+                                placeholder="Name" 
+                                name="name"
+                            >
+                        </div>
+                        <div class="form-group">
+                            <textarea 
+                                name="description" 
+                                placeholder="Description" 
+                                cols="5" rows="5" 
+                                class="form-control">
+                            </textarea>
+                        </div>
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-success">
+                                Create Task
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-center" style="padding-top: 10px">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    Current tasks
+                </div>
+
+                <div class="card-body">
+                    <ul class="list-group">
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
